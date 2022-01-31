16aab2504a2969025851f28142da5900ed382fb6
jared peck <jared.peck@uconn.edu>
2022-01-31 15:45:50 -0500

    Liked posts

---
+++ Web-fundementals/Liked/script.js
@@ -0,0 +1,34 @@
+var count = 9  // put the variable to start at the likes
+
+var countElement = document.getElementById("count") // set a variable count elememt to get the "count"
+
+function add() {
+    count ++;
+    countElement.innerText = count // set the inner htm to the count 
+    console.log(count) // log the count each time its clicked
+}
+
+
+
+var counts = 12  // put the variable to start at the likes
+
+var countsElement = document.getElementById("counter") // set a variable count elememt to get the "count"
+
+function adder() {
+    counts++;
+    countsElement.innerText =  counts// set the inner htm to the count 
+    console.log(counts) // log the count each time its clicked
+}
+
+
+  // put the variable to start at the likes
+
+var counting = 14
+var countingElement = document.getElementById("counting") // set a variable count elememt to get the "count"
+
+function adding() {
+    counting ++;
+    countingElement.innerText =  counting // set the inner htm to the count 
+    console.log(counting) // log the count each time its clicked
+}
+
